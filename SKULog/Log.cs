f5e25b810c599768f98387585574b8e73d0ffcfc
Xyunn <maxime.devecchi@gmail.com>
2018-04-15 14:42:18 -0400

    Adding the log class

---
+++ SKULog/Log.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class Log {
+
+	public static void Display(string log, GameObject gO = null)
+    {
+        Debug.Log(log, gO);
+    }
+
+    public static void Warning(string log, GameObject gO = null)
+    {
+        Debug.LogWarning(log, gO);
+    }
+
+    public static void Error(string log, GameObject gO = null)
+    {
+        Debug.LogError(log, gO);
+    }
+}
