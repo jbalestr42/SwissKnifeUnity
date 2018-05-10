554fe008b68bac831d1e15fc0caa85b9cd245e48
Xyunn <maxime.devecchi@gmail.com>
2018-05-10 18:24:01 -0400

    Serialize dictionary

---
+++ DictionarySerialized/Example.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+using SKU; 
+using UnityEngine;
+ 
+[CreateAssetMenu(menuName = "Example Asset")]
+public class Example : ScriptableObject {
+ 
+    [SerializeField]
+    private StringIntDictionary stringIntegerStore = StringIntDictionary.New<StringIntDictionary>();
+    private Dictionary<string, int> stringIntegers {
+        get { return stringIntegerStore.dictionary; }
+    }
+ 
+    [SerializeField]
+    private GameObjectFloatDictionary gameObjectFloatStore = GameObjectFloatDictionary.New<GameObjectFloatDictionary>();
+    private Dictionary<GameObject, float> screenshots {
+        get { return gameObjectFloatStore.dictionary; }
+    }
+}
