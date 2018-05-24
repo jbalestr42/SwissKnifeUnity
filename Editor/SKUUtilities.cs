5ca136f51c47a12d7b29aa3599863d25bcb910c9
Xyunn <maxime.devecchi@gmail.com>
2018-05-23 23:43:19 -0400

    Deleting Example folders

---
+++ Editor/SKUUtilities.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace SKU
+{
+    public class SKUUtilities : Editor
+    {
+
+        [MenuItem("SKU/Delete Example Folders")]
+        private static void DeleteAllExampleFolders()
+        {
+            string path = Application.dataPath + "/SKU";
+
+            RecursiveDeletion(path);
+
+            Log.Editor("Example folders deletion complete.");
+        }
+
+        private static void RecursiveDeletion(string path)
+        {
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                if (directory.Contains("Example"))
+                {
+                    Log.Editor("Folder deleted: + " + directory);
+                } else
+                {
+                    RecursiveDeletion(directory);
+                }
+            }
+        }
+    }
+}
